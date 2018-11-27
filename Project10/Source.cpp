d84c5847077fec7ef01bd0aac4ccc58ba58479da
Алиса Карпенко <@local.itstep>
2018-11-27 20:44:27 +0600

    Добавьте файлы проекта.

---
+++ Project10/Source.cpp
@@ -0,0 +1,308 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+int main()
+//{
+//	printf("Name        : “The war and the peace”\n");
+//
+//	printf("Avtor        :  L.N. Tolstoj\n");
+//
+//	printf("Izdatelstvo:  Piter\n");
+//	
+//    printf("Pages        : 500\n");
+//
+//
+//	system("pause");
+//
+//}
+
+//{
+//
+//	printf("Name       -  PP-12-1\n");
+//
+//	printf("Creator    -  student\n");
+//
+//	printf("Size       -  50MB\n");
+//	
+//    printf("Path       - D:\Academy\PP-12-1\n");
+//
+//
+//	system("pause");
+//
+//}
+
+//{
+//	int N;
+//	
+//	printf("N: ");
+//	scanf_s("%d", &N);
+//
+//	if (N % 2 == 1)
+//	{
+//		printf("no solutions\n");
+//	}
+//
+//	else if (N % 3 ==1 && N %7==0)
+//	{
+//
+//		printf("N: True\n");
+//	}
+//	else
+//	{
+//		printf("N: False\n");
+//	}
+//	
+//	system("pause");
+//
+//}
+
+//{
+//	int N;
+//
+//	printf("N: ");
+//	scanf_s("%d", &N);
+//
+//	if (N % 2 == 1)
+//	{
+//		printf("no solutions\n");
+//	}
+//
+//	else if (N % 5 == 1 && N % 4 == 1)
+//	{
+//
+//		printf("N: True\n");
+//	}
+//
+//	else
+//	{
+//		printf("N: False\n");
+//	}
+//
+//	system("pause");
+//
+//}
+
+//{
+//	int N;
+//
+//	printf("N: ");
+//	scanf_s("%d", &N);
+//
+//	if (N % 2 == 1)
+//	{
+//		printf("no solutions\n");
+//	}
+//
+//	else if (N % 8 == 0 && N % 11 == 0)
+//	{
+//
+//		printf("N: True\n");
+//	}
+//	else
+//	{
+//		printf("N: False\n");
+//	}
+//
+//	system("pause");
+//
+//}
+
+//{
+//int P;
+//float M, N;
+//
+//printf("P: ");
+//scanf_s("%d", &P);
+//
+//M = P - (float)P * 10 / 100;
+//
+//N = P - (float)P * 5 / 100;
+//
+//if (P>400 && P<600)
+//{
+//	printf("Prise: %f\n",  N);
+//}
+//
+//if(P>600 && P<1000)
+//{
+//	printf("Prise: %f\n",M);
+//}
+//
+//system("pause");
+//
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//float p, s, s2, s1, x;
+//
+//printf("Прибыль : ");
+//scanf_s("%f", &p);
+//
+//printf("Себестоимость : ");
+//scanf_s("%f", &s1);
+//
+//s2 = s1 * 5 / 100;
+//
+//s = s1 - s2;
+//
+//x = p / s * 100;
+//
+//printf("Рентабельность: %f\n", x);
+//
+//
+//system("pause");
+//
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//int k, M, H;
+//
+//printf("Секунда: ");
+//scanf_s("%d", &k);
+//
+//M = k / 60;
+//
+//H = M / 60;
+//
+//printf("Минуты: %d\n",M);
+//
+//printf("это часов в минут: %d\n", H);
+//
+//system("pause");
+//
+//
+//}
+
+//{
+//	int N;
+//
+//	printf("N: ");
+//	scanf_s("%d", &N);
+//
+//	if (N % 2 == 0 && N >9 && N <100)
+//	{
+//		printf("True\n");
+//	}
+//	
+//	else
+//	{
+//		printf("False\n");
+//	}
+//
+//	
+//
+//	system("pause");
+//
+//}
+
+//{
+// setlocale(LC_ALL,"");
+//
+//	int A,B;
+//
+//	printf("A: ");
+//	scanf_s("%d", &A);
+//
+//	printf("B: ");
+//	scanf_s("%d", &B);
+//
+//	if (A % 2 == 0 )
+//	{
+//		printf("B:нечетное \n");
+//	}
+//	
+//	else
+//	{
+//		printf("A:нечетное \n");
+//	}
+//
+//	
+//
+//	system("pause");
+//
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//int A, B;
+//
+//printf("A: ");
+//scanf_s("%d", &A);
+//
+//printf("B: ");
+//scanf_s("%d", &B);
+//
+//if (A % 2 == 1)
+//{
+//	printf("A,B:нечетное \n");
+//}
+//
+//else
+//{
+//	printf("A,B:четное \n");
+//}
+//
+//
+//
+//system("pause");
+//
+//}
+
+//{
+//
+//int N;
+//
+//printf("N: ");
+//scanf_s("%d", &N);
+//
+//if (N % 2 == 1)
+//{
+//	printf("no solutions \n");
+//
+//}
+//
+//else if (N % 7 == 0 && N % 11 >0 && N % 13 >0)
+//{
+//	printf("true \n");
+//}
+//
+//else
+//{
+//	printf("false \n");
+//}
+//
+//system("pause");
+//
+//}
+
+{
+
+	int N;
+
+	printf("N: ");
+	scanf_s("%d", &N);
+
+	 if (N % 7 == 0 && N % 3 > 0 )
+	{
+		printf("true \n");
+	}
+
+	if (N % 5 > 0 && N % 4 > 0 )
+	{
+		printf("true \n");
+	}
+
+	if (N % 8==0 && N % 11== 0)
+	{
+		printf("true \n");
+	}
+
+	system("pause");
+
+}
